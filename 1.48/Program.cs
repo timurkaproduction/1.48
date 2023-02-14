bd421107b9e1c53e8252a7f1f44f2cc9988ac788
89610 <89610@DESKTOP-VTT1R58>
2023-02-15 00:45:43 +0300

    Добавьте файлы проекта.

---
+++ 1.48/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Mainclass
+{
+    public static void Main()
+    {
+        int fund1, fund2, high;
+        Console.WriteLine("Введите длину меньшего основания:");
+        fund1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите длину большего основания:");
+        fund2 = int.Parse(Console.ReadLine());
+        if (fund1 < fund2)
+        {
+            Console.WriteLine("Введите высоту трапеции:");
+            high = int.Parse(Console.ReadLine());
+            double wall = Math.Sqrt(Math.Pow((fund2 - fund1) / 2, 2) + Math.Pow(high, 2));
+            double per = fund1 + fund2 + wall * 2;
+            Console.WriteLine("Периметр основания равен {0}", per);
+        }
+    }
+}
